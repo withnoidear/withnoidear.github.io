d4d55c488bd9d9eb84aa3743fbd753397f2191ce
Withnoidea <2482454653@qq.com>
2024-11-27 05:04:22 +0800

    Site updated: 2024-11-27 05:04:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/26/test/","2024/11/26/hello-world/","2024/11/26/测试/"];function toRandomPost(){
+var posts=["2024/11/26/hello-world/","2024/11/26/test/","2024/11/27/减肥日记/","2024/11/26/测试/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
