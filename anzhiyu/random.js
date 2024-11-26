7a0487a67127a12d33a599456e09e6734e391f04
withnoidear <2482454653@qq.com>
2024-11-26 11:47:26 +0800

    Site updated: 2024-11-26 11:47:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/26/hello-world/"];function toRandomPost(){
+var posts=["2024/11/26/241126(测试)/","2024/11/26/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
